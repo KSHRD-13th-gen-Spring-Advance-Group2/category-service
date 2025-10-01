dafbf7a4a46bf7c1aa350fe586c6c4ca4acd2ebb
Horn Narin <135811993+NarinHorn@users.noreply.github.com>
2025-10-02 00:44:17 +0700

    add resilence4j

--- src/main/java/com/hrd/categoryservice/client/UserClient.java
+++ src/main/java/com/hrd/categoryservice/client/UserClient.java
@@ -2,8 +2,10 @@ package com.hrd.categoryservice.client;
 
 import com.hrd.categoryservice.model.dto.response.ApiResponse;
 import com.hrd.categoryservice.model.entity.User;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -12,5 +14,15 @@ public interface UserClient {
 
     @SecurityRequirement(name = "bearerAuth")
     @GetMapping
+    @CircuitBreaker(name = "category-service", fallbackMethod = "userFallback")
     ResponseEntity<ApiResponse<User>> getUser();
+
+    default ResponseEntity<ApiResponse<User>> userFallback(Throwable throwable) {
+        System.out.println("Error: " + throwable.getMessage());
+        ApiResponse<User> response = ApiResponse.<User>builder()
+                .payload(null)
+                .build();
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
 }
